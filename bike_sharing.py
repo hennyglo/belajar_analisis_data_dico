7a3603c8473464c41e3453d2b7e7d17ae34dc9a6
Henny G <m281d4kx1863@bangkit.academy>
2024-06-08 15:52:39 +0700

    Update bike_sharing.py

--- bike_sharing.py
+++ bike_sharing.py
@@ -2,11 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
-from babel.numbers import format_currency
+import plotly.express as px
 from datetime import datetime, timedelta
 
-# Mengatur gaya plot seaborn
-sns.set(style='whitegrid')
+sns.set(style='dark')
 
 day_df = pd.read_csv("day.csv")
 
@@ -37,25 +36,17 @@ col1, col2 = st.columns(2)
 
 # Scatter plot untuk Musim
 with col1:
-    fig, ax = plt.subplots(figsize=(10, 6))
     season_mapping = {1: 'Springer', 2: 'Summer', 3: 'Fall', 4: 'Winter'}
     day_df['season_name'] = day_df['season'].map(season_mapping)
-    sns.barplot(x='season_name', y='cnt', data=day_df, palette="pastel")
-    ax.set_title('Rata-rata Jumlah Sewa Sepeda Berdasarkan Musim')
-    ax.set_xlabel('season')
-    ax.set_ylabel('Rata-rata Jumlah Sepeda')
-    st.pyplot(fig)
+    fig = px.bar(day_df, x='season_name', y='cnt', title='Rata-rata Jumlah Sewa Sepeda Berdasarkan Musim')
+    st.plotly_chart(fig)
 
 # Scatter plot untuk Kondisi Cuaca
 with col2:
-    fig, ax = plt.subplots(figsize=(10, 6))
     weathersit_mapping = {1: 'Few clouds', 2: 'Mist', 3: 'Light Snow', 4: 'Snow + Fog'}
     day_df['weathersit_name'] = day_df['weathersit'].map(weathersit_mapping)
-    sns.barplot(x='weathersit_name', y='cnt', data=day_df, palette="pastel")  
-    ax.set_title('Rata-rata Jumlah Sewa Sepeda Berdasarkan Kondisi Cuaca')
-    ax.set_xlabel('weathersit')
-    ax.set_ylabel('Rata-rata Jumlah Sepeda')
-    st.pyplot(fig)
+    fig = px.bar(day_df, x='weathersit_name', y='cnt', title='Rata-rata Jumlah Sewa Sepeda Berdasarkan Kondisi Cuaca')
+    st.plotly_chart(fig)
 
 # Membagi layar menjadi dua kolom untuk suhu dan suhu terasa
 col1, col2 = st.columns(2)
@@ -63,71 +54,58 @@ col1, col2 = st.columns(2)
 # Scatter plot untuk Suhu
 with col1:
     sample_df = day_df.sample(10)
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.barplot(x='temp', y='cnt', data=sample_df, palette="pastel")
-    ax.set_title('Rata-rata Jumlah Sewa Sepeda Berdasarkan Suhu')
-    ax.set_xlabel('temp')
-    ax.set_ylabel('Rata-rata Jumlah Sepeda')
-    st.pyplot(fig)
+    fig = px.bar(sample_df, x='temp', y='cnt', title='Rata-rata Jumlah Sewa Sepeda Berdasarkan Suhu')
+    st.plotly_chart(fig)
 
 # Scatter plot untuk Suhu Terasa
 with col2:
     sample_df = day_df.sample(10)
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.barplot(x='atemp', y='cnt', data=sample_df, palette="pastel")
-    ax.set_title('Rata-rata Jumlah Sewa Sepeda Berdasarkan Suhu Terasa')
-    ax.set_xlabel('atemp')
-    ax.set_ylabel('Rata-rata Jumlah Sepeda')
-    st.pyplot(fig)
-
-
+    fig = px.bar(sample_df, x='atemp', y='cnt', title='Rata-rata Jumlah Sewa Sepeda Berdasarkan Suhu Terasa')
+    st.plotly_chart(fig)
 
 st.markdown('### Korelasi antara Jumlah Sepeda Sewaan dengan Suhu, Kelembaban, dan Kecepatan Angin')
-# Membagi layar menjadi dua kolom untuk msuhu dan kelembaban
-col1, col2, = st.columns(2)
+# Membagi layar menjadi dua kolom untuk suhu dan kelembaban
+col1, col2 = st.columns(2)
 
 # Scatter plot untuk Korelasi Suhu vs Jumlah Sewa Sepeda
 with col1:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.scatterplot(x='temp', y='cnt', data=day_df, palette="pastel")
-    ax.set_title('Korelasi Suhu vs Jumlah Sewa Sepeda')
-    ax.set_xlabel('temp')
-    ax.set_ylabel('Jumlah Sepeda')
-    st.pyplot(fig)
+    fig = px.scatter(day_df, x='temp', y='cnt', title='Korelasi Suhu vs Jumlah Sewa Sepeda')
+    st.plotly_chart(fig)
 
 # Scatter plot untuk Korelasi Kelembaban vs Jumlah Sewa Sepeda
 with col2:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.scatterplot(x='hum', y='cnt', data=day_df, palette="pastel")
-    ax.set_title('Korelasi Kelembaban vs Jumlah Sewa Sepeda')
-    ax.set_xlabel('hum')
-    ax.set_ylabel('Jumlah Sepeda')
-    st.pyplot(fig)
+    fig = px.scatter(day_df, x='hum', y='cnt', title='Korelasi Kelembaban vs Jumlah Sewa Sepeda')
+    st.plotly_chart(fig)
 
 # Scatter plot untuk Korelasi Kecepatan Angin vs Jumlah Sewa Sepeda
 col1, = st.columns(1)
 with col1:
-    fig, ax = plt.subplots(figsize=(10, 6))
-    sns.scatterplot(x='windspeed', y='cnt', data=day_df, palette="pastel")
-    ax.set_title('Korelasi Kecepatan Angin vs Jumlah Sewa Sepeda')
-    ax.set_xlabel('windspeed')
-    ax.set_ylabel('Jumlah Sepeda')
-    st.pyplot(fig)
+    fig = px.scatter(day_df, x='windspeed', y='cnt', title='Korelasi Kecepatan Angin vs Jumlah Sewa Sepeda')
+    st.plotly_chart(fig)
 
 st.markdown('### Jumlah Sewa Sepeda Selama Dua Tahun Terakhir')
 # Membagi layar menjadi dua kolom untuk hari dan bulan
-col1, col2, = st.columns(2)
+col1, col2 = st.columns(2)
 
 # Scatter plot untuk Jumlah Sewa Sepeda Berdasarkan hari dan bulan
 with col1:
-    fig, ax = plt.subplots(figsize=(10, 6))
     weekday_mapping = {0: 'Sunday', 1: 'Monday', 2: 'Tuesday', 3: 'Wednesday', 4: 'Thursday', 5: 'Friday', 6: 'Saturday'}
     day_df['weekday_name'] = day_df['weekday'].map(weekday_mapping)
-    sns.barplot(x='weekday_name', y='cnt', data=day_df, palette="pastel")  
-    ax.set_title('Rata-rata Jumlah Sewa Sepeda Setiap Harinya')
-    ax.set_xlabel('weekday')
-    ax.set_ylabel('Rata-rata Jumlah Sepeda')
-    st.pyplot(fig)
+    fig = px.bar(day_df, x='weekday_name', y='cnt', title='Rata-rata Jumlah Sewa Sepeda Setiap Harinya')
+    st.plotly_chart(fig)
 
 with col2:
-    fig, ax = plt
+    month_mapping = {
+        1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
+        7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'
+    }
+    day_df['month_name'] = day_df['mnth'].map(month_mapping)
+    fig = px.bar(day_df, x='month_name', y='cnt', title='Rata-rata Jumlah Sewa Sepeda Setiap Bulan')
+    st.plotly_chart(fig)
+
+# Scatter plot untuk Jumlah Sewa Sepeda pada setiap Bulan dalam Dua Tahun Terakhir
+col1, = st.columns(1)
+with col1:
+    fig = px.line(day_df, x='mnth', y='cnt', color='yr', title='Jumlah Sewa Sepeda Tiap Bulan dalam 2 Tahun Terakhir')
+    fig.update_layout(xaxis_title='Month', yaxis_title='Jumlah Sewa Sepeda', legend_title='Year')
+    st.plotly_chart(fig)
